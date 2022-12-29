d08bdea60a0ae810cc3f961984bbc9541ce0cc77
张坤 <929794551@qq.com>
2022-12-30 00:24:56 +0800

    anchor-dark.svg

--- assets/system.js
+++ assets/system.js
@@ -1,11 +1,20 @@
 const mode = document.querySelector('#color-scheme-toggle');
-if(eval(localStorage.dark)) {
-	document.body.classList.add('dark-mode');
-	mode.src = mode.src.replace(/(\w+)(.svg)/, 'light$2');
+if (eval(localStorage.dark)) {
+  document.body.classList.add('dark-mode');
+  mode.src = mode.src.replace(/(\w+)(.svg)/, 'light$2');
+  document.querySelectorAll('img[src="assets/anchor.svg"]').forEach(v => v.src = 'assets/anchor-dark.svg');
+  document.querySelectorAll('img[src="../assets/anchor.svg"]').forEach(v => v.src = '../assets/anchor-dark.svg');
 }
 mode.addEventListener('click', function(e) {
-	e.stopPropagation();
-	const dark = document.body.classList.toggle('dark-mode');
-	this.src = this.src.replace(/(\w+)(.svg)/, (dark ? 'light' : 'dark') + '$2');
-	localStorage.dark = dark;
+  e.stopPropagation();
+  const dark = document.body.classList.toggle('dark-mode');
+  this.src = this.src.replace(/(\w+)(.svg)/, (dark ? 'light' : 'dark') + '$2');
+  if (dark) {
+    document.querySelectorAll('img[src="assets/anchor.svg"]').forEach(v => v.src = 'assets/anchor-dark.svg');
+    document.querySelectorAll('img[src="../assets/anchor.svg"]').forEach(v => v.src = '../assets/anchor-dark.svg');
+  } else {
+    document.querySelectorAll('img[src="assets/anchor-dark.svg"]').forEach(v => v.src = 'assets/anchor.svg');
+    document.querySelectorAll('img[src="../assets/anchor-dark.svg"]').forEach(v => v.src = '../assets/anchor.svg');
+  }
+  localStorage.dark = dark;
 });
